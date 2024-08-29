5729156144dc4a050a43641fb8180e3c886415b1
CanerYesiltas <caneryesiltas1@gmail.com>
2024-08-29 19:01:52 -0400

    some css style added

--- src/App.js
+++ src/App.js
@@ -1,40 +1,51 @@
-import React, { useState } from 'react';
-import List from './components/List';
-import data from "./helper/data"
-
+import React, { useState } from "react";
+import List from "./components/List";
+import data from "./helper/data";
 
 function App() {
- 
-  const itemsPerPage=5;
+  const itemsPerPage = 5; // her sayfada 5 veri gösterilecek
 
- const [Page, setPage] =useState(0);
+  const [page, setPage] = useState(0); // başlangıçta değerimiz 0'dır (sayfa değeri)
 
- 
+  const pageStartIndex = page * itemsPerPage; // her sayfada 5 veri var, bunu ilk değer ile çarp, sonuç 0 olur (0. datadan veri alınır)
 
+  const currentPageItems = data.slice(
+    pageStartIndex,
+    pageStartIndex + itemsPerPage
+  ); // kısacası amaç, datadan her sayfada 5'erli veri çekmek ve bunu değişkene atamak
 
-
-  const handlePagePrev =() =>{
-
+  const handlePagePrev = () => {
+    setPage((prevPage) => {
+      let newPage = prevPage - 1;
+      if (newPage < 0) {
+        newPage = Math.floor((data.length - 1) / itemsPerPage);
+      }
+      return newPage;
+    });
   };
 
-
-
-  const handlePageNext =() =>{
-
+  const handlePageNext = () => {
+    setPage((prevPage) => {
+      let newPage = prevPage + 1;
+      if (newPage > Math.floor((data.length - 1) / itemsPerPage)) {
+        newPage = 0;
+      }
+      return newPage;
+    });
   };
-
 
   return (
     <main>
       <section className="container">
-        <h3>Employee List</h3>
+        <h3>Employee List Of Starbucks</h3>
         <h5>
-          (Employees 1 to 5)
+          (Employees {pageStartIndex + 1} to{" "}
+          {pageStartIndex + currentPageItems.length})
         </h5>
         <List employeeData={currentPageItems} />
-        <div className='btns'>
-          <button onClick={handlePagePrev} >Prev</button>
-          <button onClick={handlePageNext} >Next</button>
+        <div className="btns">
+          <button onClick={handlePagePrev}>Prev</button>
+          <button onClick={handlePageNext}>Next</button>
         </div>
       </section>
     </main>
--- src/components/List.js
+++ src/components/List.js
@@ -1,16 +1,18 @@
 import React from "react";
 
-const List = () => {
+const List = ({employeeData}) => {
   return (
     <>
-      <article className="person">
-        <img src={"image"} alt={"name"} />
-        <div>
-          <h4>{"name"}</h4>
-          <p>{"email"}</p>
-          <p>{"age"} years</p>
-        </div>
-      </article>
+      {employeeData.map(({ image, name, age, email }) => (
+        <article key={email} className="person">
+          <img src={image} alt={name} />
+          <div>
+            <h4>{name}</h4>
+            <p>{email}</p>
+            <p>{age} years</p>
+          </div>
+        </article>
+      ))}
     </>
   );
 };
